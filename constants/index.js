3a1896e82553f0afd615b7e5d8b2c3fe775cd7e1
AmbienceStudios <ambiencexyz@gmail.com>
2022-11-25 15:12:34 -0800

    New Website

---
+++ constants/index.js
@@ -0,0 +1,88 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/d3g3n.png',
+    title: 'D3G3N Community',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/CoverArt.png',
+    title: 'Metaverse Song',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/MetaBannerr.png',
+    title: 'Metavate.io WebVR',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/Fashion.png',
+    title: 'NFT Fashion Week',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/SDR.png',
+    title: 'Web3 Radio Station',
+  },
+];
+
+export const startingFeatures = [
+  'Discover the best ways to use WebVR for your brand and learn how it works',
+  'Immerse yourself in a fantasy or a digital twin experience.',
+  'Create virtual experiences that reflect your brand with domains that match',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'Token Gated Expereiences',
+    subtitle:
+        'WebVR experiences, Discord roles, and Website Features are available to collectors only',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'Early Access + Upgrades',
+    subtitle:
+        'Token upgrades will be free as we grow and our token will become more advanced',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'Introducing your brand on to the blockchain',
+    subtitle:
+        'We will show you how to introduce your brand into the Web3 community',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: 'Getting started on the D3G3N paltform',
+    subtitle:
+        'Our onboarding process with getting you around platform',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'The Dos and Donts in the metaverse environment',
+    subtitle:
+        'Theres a lot to look out for in this space, here are some helpful tips',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
